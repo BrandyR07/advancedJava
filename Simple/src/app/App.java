3c4c9285189e1b34e95018a4fcb2bdff878dea41
Brandy Rohrer <brandyr07@gmail.com>
2022-08-05 13:26:44 -0400

    Added for loop

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -3,6 +3,10 @@ package app;
 public class App{
 
     public static void main(String[] args){
-        System.out.println("Hello World of Java");
+        System.out.println("Hello World of Java!");
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("I Love Programming with Java");
+        }
     }
 }
